c2452a1e4af0c74b7967777e125edd19501d0229
ikerrodriguez <ikerrodriguez@10.164.51.71>
2019-09-24 08:39:01 +0200

    Creada estructura del proyecto

---
+++ Prog III/src/Main.java
@@ -0,0 +1,19 @@
+import javax.swing.JFrame;
+
+public class Main extends JFrame {
+	
+	
+	private static final long serialVersionUID =1L;
+	public Main() {
+	this.setSize(200, 300);
+	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	//this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+	this.setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Main ma = new Main();
+	}
+
+}
